b3ab68ec9021242be396b103f1eadc9e0b75c0f5
Cicerossoares <98067396+Cicerossoares@users.noreply.github.com>
2022-01-30 16:52:42 -0300

    implementado a direçao

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,36 @@
 class Pessoa:
-    def __ini__(self, nome=None, idade=27):
+    olhos = 2
+    def __init__(self,*filhos, nome=None, idade=27):
         self.nome = nome
         self.idade = 27
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {self}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 27
+
+    @classmethod
+    def nome_e_atributos_class(cls):
+        return f'{cls} - olhos{cls.olhos}'
+
 if __name__ == '__main__' :
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    p.nome = 'Cicero'
-    print(p.nome)
+    cicero = Pessoa(nome='Cicero')
+    renzo = Pessoa(cicero, nome='renzo')
+    print(Pessoa.cumprimentar(cicero))
+    print(id(cicero))
+    print(renzo.cumprimentar())
+    print(renzo.nome)
+    print(renzo.idade)
+    cicero.sobrenome = 'Soares'
+    print(cicero.sobrenome)
+    for filho in cicero.filhos:
+        print(cicero.filhos)
+    print(cicero. __dict__)
+    print(renzo. __dict__)
+    print(Pessoa.olhos)
+    print(cicero.olhos)
+    print(Pessoa.metodo_estatico(), cicero.metodo_estatico())
+    print(Pessoa.nome_e_atributos_class(), cicero.metodo_estatico())
